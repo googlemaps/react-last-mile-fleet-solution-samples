5ae195800fa18b37833afb71faf886870130932f
Noelle <njung@google.com>
2022-05-31 17:50:52 -0700

    Revert "Bootstrapped RNW App"
    This reverts commit 2699e8ee29259f9b1d2669743cce5a664a0fccd1.

--- rnw-sample/README.md
+++
@@ -1,1 +0,0 @@
-# React Native for Web On-Demand Rides and Deliveries Sample
--- rnw-sample/src/__tests__/main.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer'
-
-import main from '../main';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<main/>).toJSON();
-  expect(tree).toMatchSnapshot();
-});
