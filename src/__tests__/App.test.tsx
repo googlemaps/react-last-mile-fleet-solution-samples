095393b97230cbc8880bff6db9855d8f15e97ed0
Noelle <njung@google.com>
2022-07-14 16:23:54 -0700

    fix: b/238687418 Package vulnerabilities
    - Uninstalls unused packages causing vulnerability warnings
    - Updates install instructions in README.md not to use "--save" flag
    - Amends paths in tests
    
    Change-Id: I597e3522b6bbf06379ea69c3e21c74e314063dd7

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from '../App';
+import App from '../../App';
 
 it('renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
